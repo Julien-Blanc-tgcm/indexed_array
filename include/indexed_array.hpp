63763124b923e2fb3e24a559c369ed9b0b1b63ad
Julien Blanc <julien.blanc@tgcm.eu>
2022-04-23 08:44:45 +0200

    Export indexed_span into lib namespace
    * not a detail, part of the api
    
    Signed-off-by: Julien Blanc <julien.blanc@tgcm.eu>

--- include/indexed_array.hpp
+++ include/indexed_array.hpp
@@ -20,6 +20,8 @@ using indexed_array = detail::indexed_array<Value, detail::to_single_indexer_t<I
 using detail::interval;
 using detail::single_value;
 using detail::union_of;
+template <typename Value, typename... Index>
+using indexed_span = detail::indexed_span<Value, detail::to_single_indexer_t<Index...> >;
 
 namespace detail
 {

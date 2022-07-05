929ce0c2a846534b397cd348e12bc3a8642d778d
Julien Blanc <julien.blanc@tgcm.eu>
2022-07-05 06:24:54 +0200

    Formatting
    * fixed formatting of some files
    
    Signed-off-by: Julien Blanc <julien.blanc@tgcm.eu>

--- tests/non_default_constructible.cpp
+++ tests/non_default_constructible.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE Indexed Array
-#include <boost/test/unit_test.hpp>
+
 #include <boost/describe.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "indexed_array.hpp"
 
@@ -20,6 +21,7 @@ BOOST_DESCRIBE_ENUM(Color, Red, Green, Blue, Black, White);
 class Test
 {
 	int value_;
+
   public:
 	explicit Test(int v) : value_{v}
 	{
@@ -33,18 +35,16 @@ class Test
 
 BOOST_AUTO_TEST_CASE(non_default_constructible)
 {
-	indexed_array<Test, Color> arr { Test{2}, Test{3}, Test{4}, Test{5}, Test{-1} };
+	indexed_array<Test, Color> arr{Test{2}, Test{3}, Test{4}, Test{5}, Test{-1}};
 	BOOST_TEST(static_cast<int>(arr[Color::Black]) == 5);
 }
 
 BOOST_AUTO_TEST_CASE(non_default_constructible_safe_arg)
 {
-	indexed_array<Test, Color> arr {
-	    safe_arg<Color::Red>(Test{2}),
-	    safe_arg<Color::Green>(Test{3}),
-	    safe_arg<Color::Blue>(Test{4}),
-	    safe_arg<Color::Black>(Test{5}),
-	    safe_arg<Color::White>(Test{-1})
-	};
+	indexed_array<Test, Color> arr{safe_arg<Color::Red>(Test{2}),
+	                               safe_arg<Color::Green>(Test{3}),
+	                               safe_arg<Color::Blue>(Test{4}),
+	                               safe_arg<Color::Black>(Test{5}),
+	                               safe_arg<Color::White>(Test{-1})};
 	BOOST_TEST(static_cast<int>(arr[Color::White]) == -1);
 }

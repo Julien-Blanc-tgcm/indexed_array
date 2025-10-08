7cc4e76f0617fa73aa604199f0f314f6151c28ed
Julien Blanc <julien.blanc@tgcm.eu>
2025-10-08 13:51:19 +0000

    Fix special characters in comments
    Signed-off-by: Julien Blanc <julien.blanc@tgcm.eu>

--- include/jbc/indexed_array.hpp
+++ include/jbc/indexed_array.hpp
@@ -1,6 +1,6 @@
-//·Copyright·2022·Julien Blanc
-//·Distributed·under·the·Boost·Software·License,·Version·1.0.
-//·https://www.boost.org/LICENSE_1_0.txt
+// Copyright 2022 Julien Blanc
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
 
 #ifndef JBC_INDEXED_ARRAY_H
 #define JBC_INDEXED_ARRAY_H
